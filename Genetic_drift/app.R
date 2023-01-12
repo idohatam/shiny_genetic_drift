0dce49e16d6c8b49da996e398b31c8f5c3e81820
idohatam <idohatam@gmail.com>
2023-01-12 12:30:34 -0800

    Added the proper code for the app.

--- Genetic_drift/app.R
+++ Genetic_drift/app.R
@@ -4,43 +4,42 @@ library(ggplot2)
 library(RColorBrewer)
 library(plotly)
 
+source("functions.R")
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  # Sidebar with slide bars
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("ne", "Effective population size", 
+                  min = 10, max = 1000000, value = 100, step = 10),
+      sliderInput("a1", "Proportion of allele A1", 
+                  min = 0.1, max = 0.9, value = 0.5, step = 0.1),
+      sliderInput("gen", "Number of generations", 
+                  min = 10, max = 5000, value = 500, step = 10),
+      sliderInput("sim", "Number of simulations", 
+                  min = 1, max = 10, value = 5, step = 1)
+    ),
+    # Main page with plot output
+    mainPanel(
+      plotlyOutput("plot")
     )
+  )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  # Run allele dynamics function with input from slide bars
+  results <- reactive({
+    allele_dynamics(input$ne, input$a1, input$gen, input$sim)
+  })
+  
+  # Generate plot using ggplot2 and convert to plotly
+  output$plot <- renderPlotly({
+    gg <- ggplot(results(), aes(x = generation, y = A1, color = as.factor(simulation))) +
+      geom_line(size = 1) +
+      scale_color_brewer(palette = "Paired")
+    ggplotly(gg)
+  })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
